51b6a65562c4856f77b94325df33d75d24b4da90
ilia akbarifard <iliaakbarifard110@gmail.com>
2015-03-20 20:24:00 +0330

    j

--- src/Avang/Startup.cs
+++ src/Avang/Startup.cs
@@ -9,6 +9,7 @@ namespace Avang
         public void Configure(IApplicationBuilder app)
         {
             // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+            //
         }
     }
 }
